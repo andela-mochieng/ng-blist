7a4a848076edaa02bb83e03ee0d6d41efbef23fa
Margaret Ochieng <margaret.ochieng@andela.com>
2016-08-19 16:12:41 +0300

    [Chore] rebuild dependencies

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -19,8 +19,8 @@ module.exports = function(defaults) {
       '@angular2-material/**/*.js',
       'angular2-jwt/**/*.+(js|js.map)',
       'ng2-toastr/**/*.+(js|js.map)',
-      'ng2-bs3-modal/**/*.+(js|js.map)',
-      'ng2-bootstrap/**/*.+(js|js.map)',
+      'ng2-bs3-modal/**/**',
+      'ng2-bootstrap/**/*',
     ]
   });
 };
