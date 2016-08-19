72c9b78ea1161e4a92b55dabd6ef33ac3a4aa9c6
Margaret Ochieng <margaret.ochieng@andela.com>
2016-08-19 17:30:10 +0300

    [fix bug on wrong imports]

--- src/app/bucketlist/bucketlist.service.ts
+++ src/app/bucketlist/bucketlist.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, HTTP_PROVIDERS } from '@angular/http';
-import {Observable} from 'rxjs/observable';
+import { Observable } from 'rxjs/rx';
 import { Bucketlist } from './blist';
 import 'rxjs/Rx';
 import 'rxjs/add/operator/map';
